82a6c24ce89c91a9e979e6d7b6822158b3dcc7e8
Josh Watts <josh.watts@gmail.com>
2019-10-06 21:24:59 -0600

    master: cleanup

--- spec/models/ical.spec.ts
+++ spec/models/ical.spec.ts
@@ -1,19 +1,15 @@
-import { iCal } from '../../vcardz/models/ical';
+import {
+  Event,
+  iCal,
+} from '../../vcardz/models/ical';
 import { iCalReader } from '../../vcardz/io';
 import { TestData } from '../data/testdata';
 
 
 describe('iCal', () => {
-  // it('sanity check', () => {
-  //   let cal = iCal.create();
-  //   expect(cal.toString()).toEqual(['BEGIN:VCALENDAR','VERSION:2.0','END:VCALENDAR'].join('\n'));
-  // });
-
   it('parse test', () => {
     let cal = iCalReader.fromString(TestData.event1.split('\n'));
-    // console.info(`\n${cal.toString()}`);
-    console.info(`\n${JSON.stringify(cal, null, 4)}`);
+    expect(cal.VEVENT[0] instanceof Event).toEqual(true);
   })
-
 
 });
